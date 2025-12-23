b96c3826f035454afca55d25400bef013b248d96
20210924am <jss229510@gmail.com>
2025-12-23 12:54:01 +0900

    프론트엔드 코드 초기 업로드

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//로그인 여부 등 전역상태 관리
+const AuthContext = () => {
+  return (
+    <div>AuthContext</div>
+  )
+}
+
+export default AuthContext
---
+++ frontend/src/pages/Community/Community.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import './Community.css';
+
+const Community = () => {
+
+  return (
+    <div>
+      Community
+    </div>
+  )
+}
+
+export default Community
