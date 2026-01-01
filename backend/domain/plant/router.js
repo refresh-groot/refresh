ccfa76518bce493dcf23500bfb7823c69a784cd1
ByeongyoonLee <lby020506@gmail.com>
2026-01-02 00:33:01 +0900

    필드명 다 받도록 변경

--- backend/domain/plant/router.js
+++ backend/domain/plant/router.js
@@ -1,8 +1,14 @@
+// backend/domain/plant/router.js
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
+const upload = require('../../utils/multer'); // utils 폴더의 multer 설정 불러오기
 
-router.post('/', controller.addPlant);
+// 1. 식물 등록: 사진('img')을 먼저 업로드한 후 컨트롤러 실행
+// 변경: 어떤 필드명이든 일단 다 받도록 .any()로 변경
+router.post('/', upload.any(), controller.addPlant);
+
+// 2. 식물 삭제: 기존 기능 유지
 router.delete('/:id', controller.removePlant);
 
 module.exports = router;
