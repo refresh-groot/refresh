c1adac9bb02258a889a79f09b3d98dc8852f1bf2
ByeongyoonLee <lby020506@gmail.com>
2026-01-05 23:34:05 +0900

    서버주소 업로드

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,5 +1,5 @@
 // 백엔드(Node.js) 서버 주소 파일 올리기 위한 예시임
-export const SERVER_URL = "http://localhost:8080"; 
+export const SERVER_URL = "http://223.130.157.123:8080"; 
 
 // 2. 앱 기본 정보
 export const APP_NAME = "Refresh";
