c1c921dffd804e823d678f9167692abc35fbda72
ByeongyoonLee <lby020506@gmail.com>
2026-01-15 00:41:52 +0900

    로컬호스트 설정으로 원상복구

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -192,7 +192,7 @@ function Profile() {
         )}
         <div className="item-img-box">
           <img
-            src={`${plant.photo_url}`}
+            src={`http://localhost:8080${plant.photo_url}`}
             alt={plant.species}
           />
         </div>
