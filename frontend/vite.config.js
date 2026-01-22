ed9cfa80046421bad8e0a2796d973277f69a4939
ByeongyoonLee <lby020506@gmail.com>
2026-01-22 16:10:32 +0900

    로컬호스트로 변경

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,15 +7,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-   proxy: {
-  '/api': {
-    target: 'http://223.130.157.123:8080',
-    changeOrigin: true,
-  },
-  '/uploads': {
-    target: 'http://223.130.157.123:8080',
-    changeOrigin: true,
-  }
-}
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
