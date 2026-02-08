51c44942f28d38fa7637fd84cd5df12099ff685d
rlatmdgh0531 <rlatmdgh0531@gmail.com>
2026-02-08 15:09:11 +0900

    Delete esp32_test directory

--- esp32_test/src/sensor.cpp
+++
@@ -1,9 +0,0 @@
-#include "sensor.h"
-
-void sensorInit() {
-    analogReadResolution(12); // ESP32-S3 0~4095 범위 설정
-}
-
-int getMoisture() {
-    return analogRead(SOIL_PIN);
-}
--- esp32_test/src/sensor.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-#define SOIL_PIN 8  // 센서 AOUT이 연결된 GPIO 번호
-
-void sensorInit();
-int getMoisture();
