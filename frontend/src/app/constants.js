edfd7398c27b8736a866438ba4abdddffeb40065
ByeongyoonLee <lby020506@gmail.com>
2026-03-31 18:25:11 +0900

    로컬호스트 -> 서버 연동으로 방식 변경

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,14 +1,11 @@
-// 백엔드(Node.js) 서버 주소 파일 올리기 위한 예시임
-export const SERVER_URL =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:8080'
-    : 'http://223.130.157.123:8080';
+// [수정] 모드에 상관없이 항상 클라우드 서버 주소를 사용하도록 변경
+export const SERVER_URL = "";
 
 // 2. 앱 기본 정보
 export const APP_NAME = "Refresh";
 export const APP_VERSION = "v1.0.0";
 
-// 3. 디자인 시스템 (색상 통일)
+// 3. 디자인 시스템
 export const MAIN_COLOR = "#26A69A";
 export const SUB_COLOR = "#28a745";
 
