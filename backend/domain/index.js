f87040bc1d4ad4b87abcb2802e9bdee351641efb
ByeongyoonLee <lby020506@gmail.com>
2026-05-11 15:18:14 +0900

    서버 중지 문제 해결

--- backend/domain/index.js
+++ backend/domain/index.js
@@ -26,7 +26,7 @@ const sequelize = new Sequelize(
 const User = require('./user/User');
 const Plant = require('./plant/Plant');
 const SpeciesInfo = require('./plant/SpeciesInfo'); // 1. 추가
-const Device = require('./device/Device');
+// const Device = require('./device/Device'); // [주석 처리]
 const DiagnosisLog = require('./diagnosisLog/DiagnosisLog');
 const WateringLog = require('./wateringLog/WateringLog');
 const EnvironmentLog = require('./EnvironmentLog/EnvironmentLog'); // [추가]
@@ -35,7 +35,7 @@ const Notification = require('./notification/Notification'); // [추가]
 db.User = User;
 db.Plant = Plant;
 db.SpeciesInfo = SpeciesInfo; // 2. 추가
-db.Device = Device;
+// db.Device = Device; // [주석 처리]
 db.DiagnosisLog = DiagnosisLog;
 db.WateringLog = WateringLog;
 db.EnvironmentLog = EnvironmentLog; // [추가]
@@ -45,7 +45,7 @@ db.Notification = Notification; // [추가]
 User.init(sequelize);
 Plant.init(sequelize);
 SpeciesInfo.init(sequelize); // 3. 추가
-Device.init(sequelize);
+// Device.init(sequelize); // [주석 처리]
 DiagnosisLog.init(sequelize);
 WateringLog.init(sequelize);
 EnvironmentLog.init(sequelize); // [추가]
@@ -56,7 +56,7 @@ Plant.associate(db);
 // SpeciesInfo는 정적 데이터라 현재 관계 설정이 없으면 생략 가능하지만
 // 형식을 맞추기 위해 추가(SpeciesInfo.js에 static associate가 있어야 함)
 if (SpeciesInfo.associate) SpeciesInfo.associate(db); // 4. 추가
-Device.associate(db);
+// Device.associate(db); // [주석 처리]
 DiagnosisLog.associate(db);
 WateringLog.associate(db);
 EnvironmentLog.associate(db); // [추가]
